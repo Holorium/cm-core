6a4d5847a4ad7b2a188e37912fa76fea18958890
Richard Petersen <richard.petersen@hsnet.de>
2018-01-18 19:01:22 +0100

    Add vuejs

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,31 @@ module.exports = {
     context: path.resolve(__dirname, 'src'),
     entry: {
         app: ['./index.js'],
-        vendor: ['jquery', 'underscore'],
+        vendor: ['jquery', 'underscore', 'vue'],
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "eslint-loader",
-                options: {
-                    // eslint options (if necessary)
-                }
-            },
-        ],
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+                'babel-loader',
+                'eslint-loader',
+            ]
+        }],
     },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor',
             minChunks: Infinity,
         })
-    ]
+    ],
+    resolve: {
+        alias: {
+          'vue$': 'vue/dist/vue.esm.js'
+        }
+    }
 };
